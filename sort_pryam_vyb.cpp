492b4fc8d2f52efb67852441dad1f5ad8f688cfd
s.ischanova <s.ischanova@thecoders.ru>
2021-03-06 12:16:32 +0500

    разбор остальных видов сотрировки

--- sort_pryam_vyb.cpp
+++ sort_pryam_vyb.cpp
@@ -8,16 +8,16 @@ int main()
 	int array[length] = { 30, 50, 20, 10, 40 };
  
 	// Перебираем каждый элемент массива (кроме последнего, он уже будет отсортирован к тому времени, когда мы до него доберемся)
-	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
+	for (int startIndex = 0; startIndex < length - 1; ++startIndex)// поэтому length - 1
 	{
 		// В переменной smallestIndex хранится индекс наименьшего значения, которое мы нашли в этой итерации.
-		// Начинаем с того, что наименьший элемент в этой итерации - это первый элемент (индекс 0)
+		// Начинаем с того, что псевдонаименьший элемент в этой итерации - это первый элемент (индекс 0)
 		int smallestIndex = startIndex;
  
 		// Затем ищем элемент поменьше в остальной части массива
 		for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
 		{
-			// Если мы нашли элемент, который меньше нашего наименьшего элемента,
+			// Если мы нашли элемент, который меньше нашего "наименьшего" элемента,
 			if (array[currentIndex] < array[smallestIndex])
 				// то запоминаем его
 				smallestIndex = currentIndex;
