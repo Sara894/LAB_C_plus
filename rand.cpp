9d375b9e5fb569ab1bef6c3e92da544f46eb5d9d
s.ischanova <s.ischanova@thecoders.ru>
2021-03-07 17:17:01 +0500

    RAND %

--- rand.cpp
+++ rand.cpp
@@ -8,7 +8,7 @@ int main(){
     int arr[5];
     srand(25);
     for (int i = 0; i< 5; i++)
-       arr[i] = rand()%45;
+       arr[i] = rand()%45 + 5; // число от 5 до 50
     for (int i = 0;i<5; i++)
        cout<< arr[i]<<endl;
 }
