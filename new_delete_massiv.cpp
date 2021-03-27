04dd24b7d6d0f6884e7009aacbe8b04b6b3aaf41
s.ischanova <s.ischanova@thecoders.ru>
2021-03-27 22:19:38 +0500

    Задание массива указателей

---
+++ new_delete_massiv.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //объявление массива динамического
+    //с помощью указателя и операторов new delete
+    int* p = new int[3];
+    p[1] = 100;
+    cout << p<< endl;//0x1f1940
+    cout << *p<< endl;//-1163005939
+    cout << p[0]<< endl;//-1163005939
+    cout << p[1]<< endl;//100
+    cout << p[2]<< endl;//-1163005939
+}
