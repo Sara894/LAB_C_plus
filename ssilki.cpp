cf1fee8dea62271cd7cff193ce413d5176ee6673
s.ischanova <s.ischanova@thecoders.ru>
2021-03-28 00:01:59 +0500

    равильная работа с функциями

--- ssilki.cpp
+++ ssilki.cpp
@@ -2,24 +2,20 @@
 using namespace std;
 //пишем фунцию swap
 
-int swap(int &x,int &y){
+void swap(int &x,int &y){
     int t = x;
     x = y;
     y = t;
-    return x,y;
-
-   
     //но это же локальные переменные, что делвть
 }
 int main(){
     int x =10;
     int y = 30;
-    int swap(int x,int y);
     cout<<"X: "<< x<< endl;//10 ничего не меняется)
     cout<<"Y: "<< y<< endl;//30 ничего не меняется)
-    //ЧТО ДЕЛАТЬ?!
+   
     // использовать ссылки &?
-    int swap(int &x,int &y);
+     swap(x,y);
       cout<<"X: "<< x<< endl;//опять 10 ничего не меняется)
     cout<<"Y: "<< y<< endl;//опять 30 ничего не меняется)
     // нужно менять функцию swap чтобы принимала указатели
